234be179c08eed5f3d9928894b58de5cf2273432
lmaka <lmaka94@hotmail.com>
2023-03-09 15:43:28 -0500

    Updated US54

---
+++ src/test/java/com/CRM_project/pages/ProfileNamePage.java
@@ -0,0 +1,17 @@
+package com.CRM_project.pages;
+
+import com.CRM_project.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProfileNamePage extends BasePage{
+
+    public ProfileNamePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+
+}
