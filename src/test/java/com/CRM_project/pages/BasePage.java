2a7eaec4e3d301b7bccc06180b7d51ea7404d0ab
Hazrat Shah <shah_pakhtyal@live.com>
2023-03-09 10:55:06 -0500

    US58_Shah added more new changes

--- src/test/java/com/CRM_project/pages/BasePage.java
+++ src/test/java/com/CRM_project/pages/BasePage.java
@@ -1,14 +1,10 @@
 package com.CRM_project.pages;
 
-import com.CRM_project.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
-    public BasePage() {
-        PageFactory.initElements(Driver.getDriver(), this);
-    }
+
 
     public BasePage() {
         PageFactory.initElements(Driver.getDriver(), this);
